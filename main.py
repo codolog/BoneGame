cfc025d07b420761bec9a5d28228934c094d44db
codolog <77565375+codolog@users.noreply.github.com>
2021-02-14 15:16:40 +0400

    Add files via upload

--- main.py
+++ main.py
@@ -20,6 +20,41 @@ bone_1 = randint(1, 6)
 bone_2 = randint(1, 6)
 print(bone_1, bone_2)
 
+
+# 1
+def one(x, y, bone):
+    painter.circle(screen, bone.color, \
+                   (bone.x+bone.w/2, bone.y+bone.h/2), 15, 3)
+# 2
+def two(x, y, bone):
+    painter.circle(screen, bone.color, \
+                   (bone.x+bone.w/4, bone.y+bone.h/4), 15, 3)
+    painter.circle(screen, bone.color, \
+                   (bone.x+3*bone.w/4, bone.y+3*bone.h/4), 15, 3)
+# 3
+def three(x, y, bone):
+    one(x, y, bone)
+    two(x, y, bone)
+    
+# 4
+def four(x, y, bone):
+    painter.circle(screen, bone.color, \
+                   (bone.x+bone.w/4, bone.y+3*bone.h/4), 15, 3)
+    painter.circle(screen, bone.color, \
+                   (bone.x+3*bone.w/4, bone.y+bone.h/4), 15, 3)
+# 5    
+def five(x, y, bone):
+    one(x, y, bone)
+    four(x, y, bone)
+
+# 6
+def six(x, y, bone):
+    four(x, y, bone)
+    painter.circle(screen, bone.color, \
+                   (bone.x+bone.w/2, bone.y+bone.h/4), 15, 3)
+    painter.circle(screen, bone.color, \
+                   (bone.x+bone.w/2, bone.y+3*bone.h/4), 15, 3)
+
 running = True
 while running:
     screen.fill(WHITE) # Закрашиваем весь экран цветом WHITE
@@ -37,24 +72,7 @@ while running:
     painter.rect(screen, Bone.color, \
                  (Bone.x+200, Bone.y, Bone.w, Bone.h), Bone.border, 10)
     
-    # 1
-    painter.circle(screen, Bone.color, \
-                   (Bone.x+Bone.w/2, Bone.y+Bone.h/2), 15, 3)
-    # 2-3
-    painter.circle(screen, Bone.color, \
-                   (Bone.x+Bone.w/4, Bone.y+Bone.h/4), 15, 3)
-    painter.circle(screen, Bone.color, \
-                   (Bone.x+3*Bone.w/4, Bone.y+3*Bone.h/4), 15, 3)
-    # 4-5
-    painter.circle(screen, Bone.color, \
-                   (Bone.x+Bone.w/4, Bone.y+3*Bone.h/4), 15, 3)
-    painter.circle(screen, Bone.color, \
-                   (Bone.x+3*Bone.w/4, Bone.y+Bone.h/4), 15, 3)
-    # 6
-    painter.circle(screen, Bone.color, \
-                   (Bone.x+Bone.w/2, Bone.y+Bone.h/4), 15, 3)
-    painter.circle(screen, Bone.color, \
-                   (Bone.x+Bone.w/2, Bone.y+3*Bone.h/4), 15, 3)
+   
     
     
     pg.display.update() # Обновляем экран
